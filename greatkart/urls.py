4b5667c930a51e9c4f1d827674427cfafa443513
mohammad <ms19951415@gmail.com>
2022-05-11 16:12:12 +0430

    shop gk lecture 8 ended

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -9,6 +9,7 @@ from greatkart import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
