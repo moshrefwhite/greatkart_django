de6b49afd6aba931ed0b8034cd9a055edd837ca5
mohammad <ms19951415@gmail.com>
2022-05-15 15:32:57 +0430

    lecture 13 ended in second commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,12 @@
 from django.contrib import messages, auth
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib.auth.decorators import login_required
@@ -18,8 +25,22 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'Registration successful')
-            return redirect('register')
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            # messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address [rathan.kumar@gmail.com]. Please verify it.')
+            return redirect('/accounts/login/?command=verification&email=' + email)
     else:
         form = RegistrationForm()
     context = {
@@ -36,8 +57,8 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            # messages.success(request, 'you are now logged in.')
-            return redirect('home')
+            messages.success(request, 'you are now logged in.')
+            return redirect('dashboard')
         else:
             messages.error(request, 'invalid login credentials')
             return redirect('login')
@@ -49,4 +70,23 @@ def logout(request):
     messages.success(request, 'you are logged out.')
     return redirect('login')
 
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
 
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
