4a031793fcea049a790262440154456f80cae4da
moshrefwhite <96950083+moshrefwhite@users.noreply.github.com>
2022-05-17 18:38:04 +0430

    lecture 15 ended

--- accounts/views.py
+++ accounts/views.py
@@ -7,9 +7,14 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.http import HttpResponse
+
+from carts.models import Cart, CartItem
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib.auth.decorators import login_required
+from carts.views import _cart_id
+import requests
+from requests import utils
 
 
 def register(request):
@@ -56,9 +61,54 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variations = item.variations.all()
+                        ex_var_list.append(list(existing_variations))
+                        id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, 'you are now logged in.')
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            print("@"*38 , url)
+            try:
+                query = requests.utils.urlparse(url).query
+                print('query ->', query)
+                # next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
+
         else:
             messages.error(request, 'invalid login credentials')
             return redirect('login')
--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from accounts.models import Account
 from store.models import Product, Variation
 
 
@@ -12,7 +13,7 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
-    # user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variations = models.ManyToManyField(Variation, blank=True)
     cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
