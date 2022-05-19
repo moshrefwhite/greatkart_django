00109625649c1b5c0a5c74833dd094f3f6d7e2b1
moshrefwhite <96950083+moshrefwhite@users.noreply.github.com>
2022-05-19 19:38:22 +0430

    lecture 20 ended

--- store/views.py
+++ store/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from carts.models import CartItem
 from carts.views import _cart_id
 from category.models import Category
+from orders.models import OrderProduct
 from store.forms import ReviewForm
 from store.models import Product, ReviewRating
 
@@ -36,6 +37,11 @@ def store(request, category_slug=None):
     }
     return render(request, 'store/store.html', context)
 
+
+class Orderproduct:
+    pass
+
+
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
@@ -43,9 +49,23 @@ def product_detail(request, category_slug, product_slug):
 
     except Exception as e:
         raise e
+
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+
+    # get the Reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product, status=True)
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
+        'orderproduct': orderproduct,
+        'reviews': reviews,
     }
     return render(request, 'store/product_detail.html', context)
 
